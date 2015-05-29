e4856882c2135a8ea041c0c8d68623bdb92fafc0
PhilStanton <p.k.stanton@gmail.com>
2015-05-29 12:51:51 +0100

    Simple example using Express Node module (a web fwk)

--- TheBoard/server.js
+++ TheBoard/server.js
@@ -2,8 +2,12 @@
 var express = require("express");
 var app = express();
 
+// setup view engine
+app.set("view engine", "jade");
+
 app.get("/", function(req, res) {
-    res.send("<html><body><h1>Express</h1></body></html>");
+    //res.send("<html><body><h1>Express</h1></body></html>");
+    res.render("jade/index", { title: "Express + Jade" });
 });
 
 app.get("/api/users", function (req, res) {
